f73c7edbea75becbfc5acef95054d51921f9fd79
LUIZ EDUARDO DE SOUSA FERREIRA <luiz.esferreira6@senacsp.edu.br>
2025-04-01 17:30:44 -0300

    Backup de segurança

---
+++ Ecco_Casa_de_Fogoes/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿
+namespace Ecco_Casa_de_Fogoes
+{
+    partial class frmEcco
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmEcco));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pbXis = new System.Windows.Forms.PictureBox();
+            this.btnHisto = new System.Windows.Forms.Button();
+            this.btnCaixa = new System.Windows.Forms.Button();
+            this.btnEstoque = new System.Windows.Forms.Button();
+            this.btnCadastrarP = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbXis)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(193)))), ((int)(((byte)(0)))));
+            this.panel1.Controls.Add(this.pbXis);
+            this.panel1.Controls.Add(this.btnHisto);
+            this.panel1.Controls.Add(this.btnCaixa);
+            this.panel1.Controls.Add(this.btnEstoque);
+            this.panel1.Controls.Add(this.btnCadastrarP);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1137, 55);
+            this.panel1.TabIndex = 0;
+            // 
+            // pbXis
+            // 
+            this.pbXis.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbXis.BackColor = System.Drawing.Color.Transparent;
+            this.pbXis.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbXis.Image = ((System.Drawing.Image)(resources.GetObject("pbXis.Image")));
+            this.pbXis.Location = new System.Drawing.Point(1084, 3);
+            this.pbXis.Name = "pbXis";
+            this.pbXis.Size = new System.Drawing.Size(50, 50);
+            this.pbXis.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbXis.TabIndex = 5;
+            this.pbXis.TabStop = false;
+            this.pbXis.Click += new System.EventHandler(this.pbXis_Click);
+            this.pbXis.MouseEnter += new System.EventHandler(this.Entrou);
+            this.pbXis.MouseLeave += new System.EventHandler(this.Saiu);
+            // 
+            // btnHisto
+            // 
+            this.btnHisto.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnHisto.FlatAppearance.BorderSize = 0;
+            this.btnHisto.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHisto.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHisto.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(35)))), ((int)(((byte)(166)))));
+            this.btnHisto.Location = new System.Drawing.Point(507, 3);
+            this.btnHisto.Name = "btnHisto";
+            this.btnHisto.Size = new System.Drawing.Size(259, 49);
+            this.btnHisto.TabIndex = 4;
+            this.btnHisto.Text = "Histórico de Vendas";
+            this.btnHisto.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnHisto.UseVisualStyleBackColor = true;
+            this.btnHisto.MouseEnter += new System.EventHandler(this.Entrou);
+            this.btnHisto.MouseLeave += new System.EventHandler(this.Saiu);
+            // 
+            // btnCaixa
+            // 
+            this.btnCaixa.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCaixa.FlatAppearance.BorderSize = 0;
+            this.btnCaixa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCaixa.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCaixa.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(35)))), ((int)(((byte)(166)))));
+            this.btnCaixa.Location = new System.Drawing.Point(403, 3);
+            this.btnCaixa.Name = "btnCaixa";
+            this.btnCaixa.Size = new System.Drawing.Size(98, 49);
+            this.btnCaixa.TabIndex = 3;
+            this.btnCaixa.Text = "Caixa";
+            this.btnCaixa.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCaixa.UseVisualStyleBackColor = true;
+            this.btnCaixa.MouseEnter += new System.EventHandler(this.Entrou);
+            this.btnCaixa.MouseLeave += new System.EventHandler(this.Saiu);
+            // 
+            // btnEstoque
+            // 
+            this.btnEstoque.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnEstoque.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEstoque.FlatAppearance.BorderSize = 0;
+            this.btnEstoque.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEstoque.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEstoque.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(35)))), ((int)(((byte)(166)))));
+            this.btnEstoque.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEstoque.Location = new System.Drawing.Point(12, 3);
+            this.btnEstoque.Name = "btnEstoque";
+            this.btnEstoque.Size = new System.Drawing.Size(133, 49);
+            this.btnEstoque.TabIndex = 1;
+            this.btnEstoque.Text = "Estoque";
+            this.btnEstoque.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnEstoque.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnEstoque.UseVisualStyleBackColor = true;
+            this.btnEstoque.MouseEnter += new System.EventHandler(this.Entrou);
+            this.btnEstoque.MouseLeave += new System.EventHandler(this.Saiu);
+            // 
+            // btnCadastrarP
+            // 
+            this.btnCadastrarP.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCadastrarP.FlatAppearance.BorderSize = 0;
+            this.btnCadastrarP.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCadastrarP.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastrarP.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(35)))), ((int)(((byte)(166)))));
+            this.btnCadastrarP.Location = new System.Drawing.Point(151, 3);
+            this.btnCadastrarP.Name = "btnCadastrarP";
+            this.btnCadastrarP.Size = new System.Drawing.Size(246, 49);
+            this.btnCadastrarP.TabIndex = 2;
+            this.btnCadastrarP.Text = "Cadastrar Produto";
+            this.btnCadastrarP.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCadastrarP.UseVisualStyleBackColor = true;
+            this.btnCadastrarP.MouseEnter += new System.EventHandler(this.Entrou);
+            this.btnCadastrarP.MouseLeave += new System.EventHandler(this.Saiu);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(193)))), ((int)(((byte)(0)))));
+            this.panel2.Location = new System.Drawing.Point(137, 145);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(813, 55);
+            this.panel2.TabIndex = 1;
+            // 
+            // frmEcco
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(35)))), ((int)(((byte)(166)))));
+            this.ClientSize = new System.Drawing.Size(1137, 681);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmEcco";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ecco´s Casa de Fogões";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbXis)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnHisto;
+        private System.Windows.Forms.Button btnCaixa;
+        private System.Windows.Forms.Button btnCadastrarP;
+        private System.Windows.Forms.Button btnEstoque;
+        private System.Windows.Forms.PictureBox pbXis;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Ecco_Casa_de_Fogoes/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ecco_Casa_de_Fogoes
+{
+    public partial class frmEcco : Form
+    {
+        private void pbXis_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Entrou(object sender, EventArgs e)
+        {
+            try
+            {
+                ((Button)sender).BackColor = ColorTranslator.FromHtml("#FF8813");
+            }
+            catch
+            {
+                pbXis.BackColor = Color.Red;
+            }
+        }
+
+        private void Saiu(object sender, EventArgs e)
+        {
+            try
+            {
+                ((Button)sender).BackColor = ColorTranslator.FromHtml("#FAC100");
+            }
+            catch
+            {
+                pbXis.BackColor = Color.Transparent;
+            }
+        }
+    }
+}
